db2d2b671700df8ae82734ac79bd80b99cfb77ae
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-03-18 14:52:48 +0200

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,46 @@
+use school;
+
+create table students(
+	id int,
+    firstName varchar(255),
+    lastName varchar(255),
+    teacherName varchar(255)
+);
+
+create table teachers(
+	id int,
+    teacherName varchar(255),
+    subject varchar(255)
+    );
+
+
+
+insert into students
+values
+	(1, "yoko", "Ivanov", "Gogo"),
+    (2, "kiro", "Vakadinov", "Gogo"),
+    (3, "roki", "Georgiev", "Gogo");
+
+insert into teachers
+values
+	(1, "Gogo", "BG"),
+    (2, "Ziko", "EN");
+    
+update teachers
+set teachername = "Koko"
+where id = 2;
+
+delete from students
+where id = 3;
+
+select teachers.*, students.*
+from teachers
+join sudents on teachers.teacherName = students.teacherName
+where teachers.object = "BG";
+
+
+
+
+
+
+
---
+++ yoko-table.sql
@@ -0,0 +1,25 @@
+select school;
+
+create table students(
+	id int,
+    firstName varchar(255),
+    lastName varchar(255),
+    teacherName varchar(255)
+);
+
+create table teachers(
+	id int,
+    teacherName varchar(255),
+    subject varchar(255)
+    );
+
+
+use school;
+
+insert into students
+values(1, "yoko", "Ivanov", "Gogo");
+
+insert into teachers
+
+
+
